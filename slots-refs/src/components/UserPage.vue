5cb6e643a06e5e76f0894e4dd930c30d7f1f8e66
Denis <bresgas.ru@mail.ru>
2021-09-23 15:04:56 +0500

    button
    added a button

--- slots-refs/src/components/UserPage.vue
+++ slots-refs/src/components/UserPage.vue
@@ -1,15 +1,44 @@
 <template>
-  <div>
+      <b-container class="wrapper">
+          <b-button
+            type="button"
+            @click="openModal"
+          >
+            Отправить что-нибудь
+          </b-button>
+          <modal 
+            v-if="isModalOpen"
+          />
+      </b-container>
       
-  </div>
 </template>
 
 <script>
+import modal from './modal.vue'
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-export default {};
+export default {
+  components: {
+    modal
+  },
+  data() {
+      return {
+          isModalOpen: false
+      }
+  },
+  methods: {
+      openModal() {
+          this.isModalOpen = true
+      }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+    margin-top: 50px;
+}
+
 </style>
---
+++ slots-refs/src/components/modal.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>BEEE!</div>
+</template>
