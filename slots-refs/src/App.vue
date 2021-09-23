b97b00239e0b404707e0902fc139074df7458565
Denis <bresgas.ru@mail.ru>
2021-09-23 14:28:47 +0500

    basic things
    started the project itself

---
+++ slots-refs/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <UserPage />
+  </div>
+</template>
+
+<script>
+import UserPage from './components/UserPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UserPage
+  }
+}
+</script>
+
---
+++ slots-refs/src/components/UserPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
