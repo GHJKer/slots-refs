3bdf2de09b0a1b54863aebe3d928398b3103e095
GHJKer <bresgas.ru@mail.ru>
2021-09-27 01:14:48 +0600

    full lesson
    tried to code the scroll thing

--- slots-refs/src/components/UserPage.vue
+++ slots-refs/src/components/UserPage.vue
@@ -1,44 +1,70 @@
 <template>
-      <b-container class="wrapper">
-          <b-button
-            type="button"
-            @click="openModal"
-          >
-            Отправить что-нибудь
-          </b-button>
-          <modal 
-            v-if="isModalOpen"
-          />
-      </b-container>
-      
+  <b-container class="wrapper">
+    <b-button type="button" @click="openModal"> Отправить что-нибудь </b-button>
+    <modal
+      title="Важная информация"
+      @close="isModalOpen = false"
+      v-if="isModalOpen"
+    >
+      <div class="information">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+        </p>
+      </div>
+    </modal>
+  </b-container>
 </template>
 
 <script>
-import modal from './modal.vue'
+import modal from "./modal.vue";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 export default {
   components: {
-    modal
+    modal,
   },
   data() {
-      return {
-          isModalOpen: false
-      }
+    return {
+      isModalOpen: false
+    };
   },
   methods: {
-      openModal() {
-          this.isModalOpen = true
-      }
-  }
-}
+    openModal() {
+      this.isModalOpen = true;
+      this.$emit("signal")
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .wrapper {
-    margin-top: 50px;
+  margin-top: 50px;
 }
-
 </style>
--- slots-refs/src/components/modal.vue
+++ slots-refs/src/components/modal.vue
@@ -1,3 +1,87 @@
 <template>
-    <div>BEEE!</div>
+  <div class="fade-show">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ title }}</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+            @click="closeModal"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div 
+          class="modal-body"
+          @signal="scrollJump"
+          ref="modalBody"
+          >
+          <slot></slot>
+        </div>
+        <div class="modal-footer">
+          <button 
+            type="button" 
+            class="btn btn-primary"
+            :disabled="!rulesAreRead"
+          >
+            Принять
+          </button>
+          <button
+            type="button"
+            class="btn btn-secondary"
+            @click="closeModal"
+            data-dismiss="modal"
+          >
+            Закрыть
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            rulesAreRead: false
+        }
+    },
+  props: {
+    title: {
+      type: String,
+      default: "",
+    }
+  },
+  methods: {
+    scrollJump() {
+      console.log("hi")
+      const modalBody = this.$refs.modalBody
+      modalBody.scrollBy(0, modalBody.clientHeight + modalBody.scrollTop);
+    },
+    closeModal() {
+      this.$emit("close");
+    }/* ,
+    onBodyScroll() {
+        const modalBody = this.$refs.modalBody
+        if (modalBody.clientHeight + modalBody.scrollTop >= modalBody.
+            scrollHeight ) {  (это с 19ой строки - @scroll="onBodyScroll")
+            this.rulesAreRead = true
+        }
+    } */
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  display: block;
+  &-body {
+    height: 200px;
+    overflow-y: scroll;
+  }
+}
+</style>
